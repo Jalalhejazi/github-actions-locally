55a60301cfb016e310d2a44085cfe164aef35775
Jalal Hejazi (DevOps) <jazj@demant.com>
2022-07-06 12:41:41 +0200

    readme

--- tests/test-app.js
+++ tests/test-app.js
@@ -12,12 +12,12 @@ describe('GET /', () => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
-      // there should be no errors
+        
         should.not.exist(err);
-        // there should be a 200 status code
         res.status.should.equal(200);
-        // the response should be JSON
         res.type.should.equal('text/plain');
+        res.text.should.equal('Hello World');
+
         done();
       });
   });
